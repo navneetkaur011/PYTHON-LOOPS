7d6094867e119c7e431d83b1a2ab24494818fd0b
Navneet Kaur <navvkaurnanda@gmail.com>
2026-07-20 23:10:06 +0530

    added loop example

---
+++ loop_example.py
@@ -0,0 +1,14 @@
+# Simple Python Loop Example
+
+# Counting from 1 to 5 using a for loop
+print("Counting numbers:")
+for i in range(1, 6):
+    print(f"Number: {i}")
+
+# A simple while loop countdown
+print("\nCountdown:")
+countdown = 3
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+print("Done!")
