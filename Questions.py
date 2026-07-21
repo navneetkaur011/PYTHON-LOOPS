48ea683cb75da2589d3c00511816a5f3fa9f820f
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-21 23:41:02 +0530

    Added solved questions file

--- Questions.py
+++ Questions.py
@@ -1,18 +1,18 @@
 # #Print numbers from 1 to 100
-# print("----Ques-01----")
-# Numbers = 1
-# while Numbers<=100:
-#     print(Numbers)
-#     Numbers += 1
-# print("Done!")
+print("----Ques-01----")
+Numbers = 1
+while Numbers<=100:
+    print(Numbers)
+    Numbers += 1
+print("Done!")
 
 # # Print numbers from 100 to 1
-# print("----Ques-02----")
-# i = 100
-# while i>=1:
-#     print(i)
-#     i -= 1
-# print("Loop Ended!")
+print("----Ques-02----")
+i = 100
+while i>=1:
+    print(i)
+    i -= 1
+print("Loop Ended!")
 
 # Print the multiplication table of number n
 n = int(input("Enter number: "))
@@ -20,3 +20,7 @@ i = 1
 while i <= 10:
     print(n*i)
     i += 1
+print("Done!")
+
+
+    
