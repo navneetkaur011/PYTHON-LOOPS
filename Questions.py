d966f4570ef38444266e02487edbce6730790f0c
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-21 23:39:14 +0530

    Added soled questions file

---
+++ Questions.py
@@ -0,0 +1,22 @@
+# #Print numbers from 1 to 100
+# print("----Ques-01----")
+# Numbers = 1
+# while Numbers<=100:
+#     print(Numbers)
+#     Numbers += 1
+# print("Done!")
+
+# # Print numbers from 100 to 1
+# print("----Ques-02----")
+# i = 100
+# while i>=1:
+#     print(i)
+#     i -= 1
+# print("Loop Ended!")
+
+# Print the multiplication table of number n
+n = int(input("Enter number: "))
+i = 1
+while i <= 10:
+    print(n*i)
+    i += 1
