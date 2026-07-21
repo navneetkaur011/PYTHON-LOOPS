da1608cb1aed42697fdfe6ae5a8942698d49f90c
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-21 23:25:38 +0530

    Add loop code file

---
+++ 01_Loop.py
@@ -0,0 +1,14 @@
+# Loops are used to repeat instructions.
+i = 1
+while i<=10:
+    print("Good Vibes")
+    i+=1
+print("----Loop Ended!----") 
+
+# A simple while loop countdown
+print("\nCountdown:")
+countdown = 10
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+print("Done!")
