07d794aa4fa707573a5369fc54b1c1c637e95a11
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-23 23:49:48 +0530

    Add range function and pass statement implementation

--- 02_For_Loop.py
+++ 02_For_Loop.py
@@ -1,3 +1,4 @@
+print("---FOR LOOP---")
 Emotions = ["Funny", "Sad", "chill", "Rude", "Happy", "Kind"]
 for el in Emotions:
     print(el)
@@ -10,4 +11,23 @@ for el in Veggies:
         break
     print(el)
 else:
-    print("Not Found!")
+    print("Not Found!\n")
+
+""" 
+RANGE: Range function returns a sequence of numbers , starting from 0 by default and increments 
+       by 1 (by default) and stops before a specified number.
+"""
+print("---Range function---")
+for i in range(11):
+    print(i)
+print()
+
+for i in range(2,10,2):   #range(start,stop,step)
+    print(i)
+
+# PASS: It is a null statement that does nothing. It is used as a placefolder for future code. 
+print("\n---Pass statement---")
+for i in range(0,39,-2):
+    pass
+print("This block is currently empty as a placeholder for future code")
+
