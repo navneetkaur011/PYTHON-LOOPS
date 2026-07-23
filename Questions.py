0fa72e1a1a01b183059c736e95a4b6196cb62cab
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-23 23:55:13 +0530

    Add range and loop practice questions

--- Questions.py
+++ Questions.py
@@ -35,7 +35,9 @@ while i< len(Tup):
     if(Tup[i] == x):
        print("Found at Index", i)
     i += 1
-
+else:
+        print("Not found!")
+    
 print("Ques-6. Print the elements of the list using for loop.")
 list = [12, 56, 38, 22, 90, 18, 29, 58, 11, 3]
 for val in list:
@@ -45,8 +47,36 @@ print("Ques-7. Search for a number in the tuple using for loop")
 Tup = (13, 34, 67, 20, 92, 44, 10, 45, 67, 59, 67, 92)
 x = int(input("Enter number to search: "))
 i = 0
-for val in Tup:
+for val in Tup:            #Linear Search
     if(val == x):
         print("Found", x, "at index", i)
     i += 1
+else:
+    print("Not found!")
 
+print("Ques-8, Print numbers from 1 to 32 using range function")
+for i in range(33):
+    print(i)
+
+print("Ques-9. Print numbers from 32 to 1 using range function.")
+for i in range(33,0,-1):     #range(start,stop,step)
+    print(i)
+
+print("Ques-10. Print the multiplication table of number n using range function.")
+n = int(input("Enter number: "))
+for i in range(1,11):
+    print(i*n)
+
+print("Ques-11. Write a program to find the sum of natural numbers.")
+n = int(input("Enter number: "))
+sum = 0 
+for i in range(1, n+1):
+    sum += i
+print("Total Sum: ", sum)
+
+print("Ques-12. Write a program to find the factorial of first n numbers.")
+n = int(input("Enter number: "))
+Fac = 1
+for i in range(1, n+1):
+    Fac *= i
+print("Factorial of", n, "is:", Fac)
